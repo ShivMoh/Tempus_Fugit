9ecfb5bd887e4468503190a0e504aea6e5b03883
ShivMoh <shiveshmohamed@gmail.com>
2023-11-22 18:10:11 -0400

    Made the BillPreview Page a bit more responsive. Still kinda looks eh, tho...

--- inc/view/BillPreviewView.php
+++ inc/view/BillPreviewView.php
@@ -2,24 +2,23 @@
     <link rel="stylesheet" href="http://localhost/personal/Practice/oop-mvc/public/css/bill-preview.css">
 </head>
 
-<td> testing </td>
 <div class="container-sm bill-preview-container">
     <div class="row justify-content-center">
-        <div class="col-10 col-sm-9">
+        <div class="col-lg-6 col-sm-6">
             Transaction: 12314242
         </div>
-        <div class="col col-sm-auto">
+        <div class="col-lg-auto col-sm-6">
             Date: yyyy-mm-dd
         </div>
     </div>
 
     <div class="row justify-content-center">
         <div class="col text-center bill-preview-heading-1">
-            Karen's Kitchen
+            <img src="http://localhost/personal/Practice/oop-mvc/public/images/karens-kitchen-logo.png" alt="">
         </div>
     </div>
     
-    <div class="row justify-content-center">
+    <div class="row justify-content-center bill-preview-item-row">
         <div class="col-10 col-sm-9">
             Item
         </div>
@@ -29,7 +28,7 @@
         </div>
     </div>
 
-    <div class="row justify-content-center">
+    <div class="row justify-content-center bill-preview-item-row">
         <div class="col-10 col-sm-9">
             Item
         </div>
@@ -39,7 +38,7 @@
         </div>
     </div>
 
-    <div class="row justify-content-center">
+    <div class="row justify-content-center bill-preview-item-row">
         <div class="col-10 col-sm-9">
             Item
         </div>
@@ -49,7 +48,7 @@
         </div>
     </div>
 
-    <div class="row justify-content-center">
+    <div class="row justify-content-center bill-preview-item-row">
         <div class="col-10 col-sm-9">
             Item
         </div>
@@ -61,41 +60,41 @@
  
     <div class="separator"></div>
     <div class="row justify-content-center bill-preview-title-row">
-        <div class="col-8 col-sm-7"></div>
-        <div class="col-2 bill-preview-heading-3">
+        <div class="col-lg-8 col-sm-2"></div>
+        <div class="col-lg-2 col-sm-5 bill-preview-heading-3">
             Subtotal:
         </div>
-        <div class="col-2 col-sm-auto">
+        <div class="col-lg-2 col-sm-5">
             $xxxx
         </div>
     </div>
 
     <div class="row justify-content-center bill-preview-title-row">
-        <div class="col-8 col-sm-7"></div>
-        <div class="col-2 bill-preview-heading-3">
+        <div class="col-lg-8 col-sm-2"></div>
+        <div class="col-lg-2 col-sm-5 col-sm-auto bill-preview-heading-3">
             VAT:
         </div>
-        <div class="col-2 col-sm-auto">
+        <div class="col-lg-2 col-sm-5 col-sm-auto">
             $xxxx
         </div>
     </div>
 
     <div class="row justify-content-center bill-preview-title-row">
-        <div class="col-8 col-sm-7"></div>
-        <div class="col-2 bill-preview-heading-3">
+        <div class="col-lg-8 col-sm-2"></div>
+        <div class="col-lg-2 col-sm-5 bill-preview-heading-3">
             Discount:
         </div>
-        <div class="col-2 col-sm-auto">
+        <div class="col-lg-2 col-sm-5">
             $xxxx
         </div>
     </div>
 
     <div class="row justify-content-center bill-preview-title-row">
-        <div class="col-8 col-sm-7"></div>
-        <div class="col-2 bill-preview-heading-2">
+        <div class="col-lg-8 col-sm-2"></div>
+        <div class="col-lg-2 col-sm-5 bill-preview-heading-2">
             Total:
         </div>
-        <div class="col-2 col-sm-auto">
+        <div class="col-lg-2 col-2 col-sm-5 col-sm-auto">
             $xxxx
         </div>
     </div>
@@ -104,14 +103,14 @@
     
 </div>
 
-<div class="container-sm">
-    <div class="row justify-content-start bill-preview-button-container">
-        <div class="col-8 col-sm-4"></div>
-        <div class="col-2">
-            <button class="bill-preview-cancel-button">Cancel Bill</button>
-        </div>
-        <div class="col-2 col-sm-auto">
-            <button class="bill-preview-print-button">Print Bill</button>
-        </div>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-5 col-sm">
+        <button class="bill-preview-cancel-button">Cancel Bill</button>
     </div>
+    <div class="col-lg-5 col-sm">
+        <button class="bill-preview-print-button">Print Bill</button>
+    </div>
+  </div>
 </div>
