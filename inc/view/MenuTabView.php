52ac23f8e72871ac51247cc9c1e7ced46c45070e
guapion <zanebishop6978@gmail.com>
2023-11-22 12:02:13 -0500

    updated MenuTab

--- inc/view/MenuTabView.php
+++ inc/view/MenuTabView.php
@@ -1,6 +1,51 @@
 <?php
-
-    include "navbar.php";
-    echo "Menu tab"
-
+    include "NavBarView.php";
+    echo "Menu tab";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../public/css/MenuTabView.css">
+    <title>MenuTabView</title>
+</head>
+<body>
+    <div class="div">
+        <div class="div-2">
+            <img
+                loading="lazy"
+                srcset="../../public/images/img.png"
+                class="img"
+            />
+            <div class="div-3">Search Menu Item by name or id</div>
+        </div>
+        <div class="div-4">
+            <div class="div-5">Tags</div>
+            <img
+                loading="lazy"
+                src="../../public/images/img-2.png"
+                class="img-2"
+            />
+            <div class="div-6">Price</div>
+            <img
+                loading="lazy"
+                src="../../public/images/img-3.png"
+                class="img-3"
+            />
+        </div>
+        <img
+            loading="lazy"
+            srcset="../../public/images/img-4.png"
+            class="img-4"
+        />
+    </div>
+    <div class="rectangle"></div>
+
+
+
+</body>
+</html>
