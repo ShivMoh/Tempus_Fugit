2d58d48f3221f4f650211248d6403709e290b7b7
Xenoph3n <ricardonarine732@gmail.com>
2023-11-28 19:53:14 -0400

    Added the 2 error pages

---
+++ inc/view/errors/401.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h1 {
+            color: #e74c3c;
+            font-size: 20em;
+            margin: 0;
+        }
+
+        p {
+            color: #7f8c8d;
+            font-size: 3em;
+        }
+    </style>
+</head>
+<body>
+    <h1>401</h1>
+    <p>Unauthorized Access</p>
+    <p>You are not authorized to access this page!</p>
+</body>
+</html>
