39e634d77f6a782d3177e51b0251d64120f13c1a
Xenoph3n <ricardonarine732@gmail.com>
2023-11-28 20:12:45 -0400

    Deleted old view, added menu item form

---
+++ inc/view/MenuItemAddView.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Item Form</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name" required>
+
+        <label for="price">Price:</label>
+        <input type="text" name="price" required>
+
+        <label for="description">Description:</label>
+        <textarea name="description"></textarea>
+
+        <label for="image">Image:</label>
+        <input type="text" name="image">
+
+        <label for="discount">Discount:</label>
+        <input type="text" name="discount">
+
+        <label for="tags">Tags:</label>
+        <input type="text" name="tags">
+
+        <label for="ingredients">Ingredients:</label>
+        <input type="text" name="ingredients">
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
