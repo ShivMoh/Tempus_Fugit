04723238a5acd3449bf5a54c621bf3d300e4d0b0
Xenoph3n <ricardonarine732@gmail.com>
2023-11-29 21:47:55 -0400

    Added menu tab edit modal

--- inc/view/MenuTabView.php
+++ inc/view/MenuTabView.php
@@ -39,7 +39,6 @@
     <!-- SEARCH BAR -->
 
     <!-- SELECT * QUERY -->
-
     <?php foreach ($data as $item): ?>
         <div class="menu-item-container">
             <div class="image-and-tags">
@@ -62,7 +61,9 @@
                 <button class="action-button expand-button" data-id="<?php echo $item['id']; ?>">
                     <img class="action-button-image" src="<?= RESOURCE_URL."expand-icon.png"?>" alt="Expand" title="Expand">
                 </button>
-                <button class="action-button"><img class="action-button-image" src="<?= RESOURCE_URL."edit-icon.png"?>" alt="Edit" title="Edit"></button>
+                <button class="action-button edit-button" data-id="<?php echo $item['id']; ?>">
+                    <img class="action-button-image" src="<?= RESOURCE_URL."edit-icon.png"?>" alt="Edit" title="Edit">
+                </button>
                 
                 <form action="<?=BASE_URL."/MenuItem/delete"?>" method="post">
                     <input type="hidden" name="id" value="<?= $item['id']; ?>">
@@ -70,67 +71,103 @@
                 </form>
             </div>
         </div>
-    </div>
 
-    <!-- Modal -->
-    <div class="modal" id="modal-<?php echo $item['id']; ?>">
-        <div class="modal-content">
-            <div class="image-and-tags-modal-div">
-               
-                <img class="modal-menu-item-img" src="<?=RESOURCE_URL.$item['image'];?>" alt="<?php echo $item['name']; ?>">
-                <div class="modal-tag-buttons">
-                    <?php $tags = explode(',', $item['tags']); ?>
-                    <?php foreach ($tags as $tag): ?>
-                        <button class="modal-tag-button"><?php echo $tag; ?></button>
-                    <?php endforeach; ?>
+        <!-- Expand Modal -->
+        <div class="modal" id="modal-<?php echo $item['id']; ?>">
+            <div class="modal-content">
+                <div class="image-and-tags-modal-div">
+                    <img class="modal-menu-item-img" src="<?=RESOURCE_URL.$item['image'];?>" alt="<?php echo $item['name']; ?>">
+                    <div class="modal-tag-buttons">
+                        <?php $tags = explode(',', $item['tags']); ?>
+                        <?php foreach ($tags as $tag): ?>
+                            <button class="modal-tag-button"><?php echo $tag; ?></button>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+                <div class="menu-item-details-modal-div">
+                    <p class="modal-item-name"><?php echo $item['name']; ?></p>
+                    <p class="modal-item-description"><?php echo $item['description']; ?></p>
+                    <ul class="item-ingredients">
+                        <p class="modal-item-ingredients">Ingredients</p>
+                        <?php $ingredients = explode(',', $item['ingredients']); ?>
+                        <?php foreach ($ingredients as $ingredient): ?>
+                            <li><?php echo $ingredient; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
                 </div>
             </div>
+        </div>
 
-            <div class="menu-item-details-modal-div">
-                <p class="modal-item-name"><?php echo $item['name']; ?></p>
-                <p class="modal-item-description"><?php echo $item['description']; ?></p>
-                <ul class="item-ingredients">
-                    <p class="modal-item-ingredients">Ingredients</p>
-                    <?php $ingredients = explode(',', $item['ingredients']); ?>
-                    <?php foreach ($ingredients as $ingredient): ?>
-                        <li><?php echo $ingredient; ?></li>
-                    <?php endforeach; ?>
-                </ul>
+        <!-- Edit Modal -->
+        <div class="modal edit-modal" id="edit-modal-<?php echo $item['id']; ?>">
+            <div class="modal-content">
+                <form action="<?= BASE_URL . "/MenuItem/edit/" . $item['id']; ?>" method="post">
+                    <label for="edit-item-name">Name</label>
+                    <input type="text" id="edit-item-name" name="edit-item-name" value="<?php echo $item['name']; ?>" required>
+
+                    <label for="edit-item-description">Description</label>
+                    <input type="text" id="edit-item-description" name="edit-item-description" value="<?php echo $item['description']; ?>" required>
+
+                    <label for="edit-item-ingredients">Ingredients</label>
+                    <input type="text" id="edit-item-ingredients" name="edit-item-ingredients" value="<?php echo $item['ingredients']; ?>" required>
+
+                    <label for="edit-item-price">Price</label>
+                    <input type="number" id="edit-item-price" name="edit-item-price" value="<?php echo $item['price']; ?>" required>
+
+                    <label for="edit-item-image">Image</label>
+                    <input type="text" id="edit-item-image" name="edit-item-image" value="<?php echo $item['image']; ?>" required>
+
+                    <label for="edit-item-discount">Discount</label>
+                    <input type="text" id="edit-item-discount" name="edit-item-discount" value="<?php echo $item['discount']; ?>" required>
+
+                    <button type="submit">Save Changes</button>
+                </form>
             </div>
         </div>
-    </div>
+    <?php endforeach; ?>
 
     <script>
         var modals = document.querySelectorAll('.modal');
+        var editModals = document.querySelectorAll('.edit-modal');
         var expandButtons = document.querySelectorAll('.expand-button');
-        var closeButtons = document.querySelectorAll('.close');
+        var editButtons = document.querySelectorAll('.edit-button');
+
+        // Function to display a modal by ID
+        function displayModal(modalId) {
+            var modal = document.getElementById(modalId);
+            modal.style.display = 'block';
+        }
 
         // Loop through each modal and add click event listeners to the expand buttons
         expandButtons.forEach(function (button) {
             button.addEventListener('click', function () {
-                var modalId = this.getAttribute('data-id');
-                var modal = document.getElementById('modal-' + modalId);
-                modal.style.display = 'block';
+                var modalId = 'modal-' + this.getAttribute('data-id');
+                displayModal(modalId);
             });
         });
 
-        // Closes the modal
-        closeButtons.forEach(function (button) {
+        // Loop through each edit button and add click event listeners to open the edit modal
+        editButtons.forEach(function (button) {
             button.addEventListener('click', function () {
-                var modalId = this.getAttribute('data-id');
-                var modal = document.getElementById('modal-' + modalId);
-                modal.style.display = 'none';
+                var editModalId = 'edit-modal-' + this.getAttribute('data-id');
+                displayModal(editModalId);
             });
         });
 
+        // Close the modal if the user clicks outside of it
         window.addEventListener('click', function (event) {
             modals.forEach(function (modal) {
                 if (event.target == modal) {
                     modal.style.display = 'none';
                 }
             });
+
+            editModals.forEach(function (modal) {
+                if (event.target == modal) {
+                    modal.style.display = 'none';
+                }
+            });
         });
     </script>
-<?php endforeach; ?>
 </body>
 </html>
