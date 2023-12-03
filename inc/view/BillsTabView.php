09b3710e141073acd4d56e3eafe8c102b31e76d0
Xenoph3n <ricardonarine732@gmail.com>
2023-12-03 10:24:28 -0400

    Changed icons bill tab view

--- inc/view/BillsTabView.php
+++ inc/view/BillsTabView.php
@@ -87,7 +87,7 @@
                                 <button class="action-button"><img src="<?= RESOURCE_URL."cancel_icon.png"?>" alt="Cancel Icon" title="Cancel"></button>
                             </form>   
                             <form class="button-form" action="<?=BASE_URL."/bill/update/".$bill['id']?>" method="POST">
-                                <button class="action-button"><img src="<?= RESOURCE_URL."edit-icon.png"?>" alt="Edit Icon" title="Mark As Completed"></button>
+                                <button class="action-button"><img src="<?= RESOURCE_URL."done-icon.png"?>" alt="Edit Icon" title="Mark As Completed"></button>
                             </form>   
                         </div>
                     </td>
