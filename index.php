22cda927d32ce479bf77ae5712a7b09ea4ee142e
Rey <reynardetwaroo4@gmail.com>
2023-12-02 22:59:52 -0400

    Fixed the main.css error.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
     </head>
     <body>
